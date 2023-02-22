4331a4124fe5b76c640766a698dc96a90526df9d
Eduardo Grajales Villanueva <edgravill@gmail.com>
2023-02-21 20:27:06 -0600

    Variables, objects & if/else

---
+++ 2_javascript_101/ifs.js
@@ -0,0 +1,40 @@
+function wrongInput(whatsNext) {
+  alert('Please enter a valid age (only digits)');
+  return whatsNext();
+}
+
+function amIAnAdult() {
+  const stringAge = prompt('What is your age?');
+
+  if (!stringAge) {
+    return wrongInput(amIAnAdult);
+  }
+
+  const isAValidAge = /^[0-9]*$/.test(stringAge);
+
+  // if (isAValidAge === false) {
+  // if (isAValidAge !== true) {
+  if (!isAValidAge) {
+    return wrongInput(amIAnAdult);
+  }
+
+  const age = parseInt(stringAge);
+
+  if (Number.isNaN(age)) {
+    return wrongInput(amIAnAdult);
+  }
+
+  if (age < 0 || age > 60) {
+    return wrongInput(amIAnAdult);
+  }
+
+  if (age >= 18) {
+    alert('You are an adult');
+  } else {
+    alert('You are a child');
+  }
+
+  return age;
+}
+
+console.log(amIAnAdult());
